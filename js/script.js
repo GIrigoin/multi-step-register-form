12fd7ed4f43e477521589aee5d66f06776df8738
GIrigoin <guilleirigoin@hotmail.com>
2024-08-14 18:23:52 -0300

    First steps - multiform

--- js/script.js
+++ js/script.js
@@ -1,2 +1,22 @@
 // Add some interactivity to the website
-console.log("Hello, world!");
+let step = 1;
+
+const handleSubmit = () => {
+  event.preventDefault();
+  if (step === 1) {
+    let subForm1 = document.getElementById("step1");
+    let subForm2 = document.getElementById("step2");
+    subForm1.style.display = "none";
+    subForm2.style.display = "block";
+    step++;
+    return false;
+  }
+  if (step === 2) {
+    let subForm3 = document.getElementById("step3");
+    let subForm2 = document.getElementById("step2");
+    subForm2.style.display = "none";
+    subForm3.style.display = "block";
+    step++;
+    return false;
+  }
+};
